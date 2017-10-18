4eac7913aad52f0727aed6a5d10fb49654f809db
jasonmotha <25143301+jasonmotha@users.noreply.github.com>
2017-10-18 16:59:40 +1300

    updated surface conversion header formatting to allow landslide

--- gen_gf_surface.py
+++ gen_gf_surface.py
@@ -23,11 +23,11 @@ import argparse
 parser = argparse.ArgumentParser(description='Convert a USGS H5 file to xyz for plotting with "plot_stations.py"')
 
 parser.add_argument('filename', help='Filename of h5 file to convert')
-
+parser.add_argument('-type', '--gftype', default='liq', choices=['liq', 'ls'], type=str)
 parser.add_argument('-m', '--model',  default=2, choices=[1,2], type=int,
                     help='Selects which model has been used: 1- coastal 2- general (default) (determines text on figures)')
 parser.add_argument('-l', '--limit', default=float('-inf'), type=float,
-                    help='Specifies a threshold value - useful for removing unnecessary fill')
+                    help='Specifies a threshold value - useful for removing unnecessary fill removes values below this point')
 parser.add_argument('-s', '--susceptibility', default=False, action="store_true", help='Changes the plot labels to indicate susceptibility ')
 parser.add_argument('-t', '--title', help='Title for the top of the graph. Defaults to a trimmed run_name')
 parser.add_argument('-o', '--output', default=None, help='sets the name of the output file') 
@@ -40,6 +40,14 @@ susceptibility = args.susceptibility
 model = args.model
 plot_title = args.title
 fout_name = args.output
+
+is_liq = is_ls = False
+if args.gftype == "liq":
+    is_liq = True
+elif args.gftype == "ls":
+    is_ls = True
+else:
+    exit("-type unset")
 
 ## Read H5 file to be converted
 
@@ -64,20 +72,25 @@ with open(fout_name, 'w') as fout:
         plot_title = basename[0:20] + '\n'
     fout.write(plot_title)
 
-    #hot:invert,t-30 1k:g-nearneighbor
-    #/home/nesi00213/qcore/plot/cpt/liquifaction-nolabel.cpt:fixed,categorical,t-40 1k:g-nearneighbor
-    fout.write('Liquefaction ')
-    if susceptibility:
-        fout.write("Susceptibility \n<REPO>/liquefaction-nolabel.cpt:topo-grey1,fixed,categorical,t-30 1k:g-nearneighbor,landmask\n\n")
+    if is_liq:
+        fout.write('Liquefaction ')
+        if susceptibility:
+            fout.write("Susceptibility \n<REPO>/liquefaction-nolabel.cpt:topo-grey1,fixed,categorical,t-30 1k:g-nearneighbor,landmask\n\n")            
     else:
+        fout.write('Landslide ')
+        if susceptibility:
+            fout.write("Susceptbility \n<REPO>/landslide_suc.cpt:topo-grey1,t-30,fixed,categorical 1k:g-nearneighbor,landmask\n-10 0 0.25 2\n")
+    if not susceptibility:
         fout.write("Probability \nhot:topo-grey1,invert,t-30 1k:g-nearneighbor,landmask\n0 1 0.02 0.1\n")
 
     fout.write("1 white\n")
 
-    if model == 1:
+    if model == 1 and is_liq:
         fout.write("Coastal Model\n")
-    else:
+    elif is_liq:
         fout.write("General Model\n")
+    else:
+        fout.write("\n")
 
     len_lon = len(lon)
     vmax = -float('inf')
