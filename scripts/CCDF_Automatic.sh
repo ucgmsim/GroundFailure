51b9a879ec9d939c86c585f798eceae8e586b7fe
LukeLongworth <luke.longworth.nz@gmail.com>
2017-12-13 14:59:09 +1300

    This automates Ford's script for CCDFs. This update just changes some filepaths and comments

--- scripts/CCDF_Automatic.sh
+++ scripts/CCDF_Automatic.sh
@@ -4,7 +4,7 @@
 # Inputs: 1) A filepath to the runs directory within which each fault is a subfolder
 #         2) A file that contains all the fault names. This will need to be written for each set of cybershake runs (eg. v17p8)
 
-# Usage: bash /home/lukelongworth/GroundFailure/scripts/CCDF_Automatic.sh /home/nesi00213/Runfolder/Cybershake/v17p8/Runs /home/nesi00213/Runfolder/Cybershake/v17p8/Runs/list_runs_v17p8
+# Usage: bash /home/lukelongworth/GroundFailure/scripts/CCDF_Automatic.sh /home/nesi00213/Runfolder/Cybershake/v17p8/Runs /home/nesi00213/Runfolder/Cybershake/v17p8/temp/list_runs_v17p8
 
 # Name the inputs
 run_dir=$1 #/home/nesi00213/RunFolder/Cybershake/v17p8/Runs - This allows us to change between simulation datasets (eg to v17p9)
@@ -16,10 +16,10 @@ for run_name in $list_runs
 do
     for realisation_path in `find  $run_dir/$run_name/Impact/Liquefaction/ -type d -name ""$run_name"_HYP*"` #Pull out the filepath to the directory for specific realisation of the chosen fault
     do
-    python Liq_ccdf_regional.py $realisation_path/*tal_probability_n*
-    python Liq_ccdf_regional.py $realisation_path/*tal_probability_t*
-    python Liq_ccdf_regional.py $realisation_path/*ral_probability_n*
-    python Liq_ccdf_regional.py $realisation_path/*ral_probability_t*
+    python Liq_ccdf_regional.py $realisation_path/*zhu_2016_coastal_probability_n*
+    python Liq_ccdf_regional.py $realisation_path/*zhu_2016_coastal_probability_t*
+    python Liq_ccdf_regional.py $realisation_path/*zhu_2016_general_probability_n*
+    python Liq_ccdf_regional.py $realisation_path/*zhu_2016_general_probability_t*
     done
 done
 
