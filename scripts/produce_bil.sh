e4ccb82f3a59df20c041944a6d0024b3541befdb
LukeLongworth <luke.longworth.nz@gmail.com>
2017-12-19 15:45:56 +1300

    This was modified to run only a small subset

--- scripts/produce_bil.sh
+++ scripts/produce_bil.sh
@@ -12,12 +12,12 @@ for run_name in $list_runs
 do
   for realisation_path in `find  $run_dir/$run_name/GM/Sim/Data/$run_name -type d -name ""$run_name"_HYP*"`
   do
-    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_general_susceptibility_nz-specific-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/General_NZ/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
-    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_general_susceptibility_topo-based-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/General_topo/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
-    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_coastal_susceptibility_nz-specific-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/Coastal_NZ/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
-    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_coastal_susceptibility_topo-based-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/Coastal_topo/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
+    #python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_general_susceptibility_nz-specific-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/General_NZ/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
+    #python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_general_susceptibility_topo-based-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/General_topo/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
+    #python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_coastal_susceptibility_nz-specific-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/Coastal_NZ/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
+    #python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/zhu_2016_coastal_susceptibility_topo-based-vs30.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Liquefaction/Coastal_topo/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
   
-    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/jessee_2017_susceptibility.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Landslide/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/liquefaction_model --gis
+    python3 /usr/bin/gfail /home/nesi00213/groundfailure/config/jessee_2017_susceptibility.ini $realisation_path/grid.xml --set-bounds 'zoom, pgv, 0' --hdf5 -o /home/lukelongworth/BILs/Landslide/ -c /home/nesi00213/groundfailure/config -d /home/nesi00213/groundfailure/landslide_model --gis
   done
 done
 
