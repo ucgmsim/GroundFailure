e525f8477f21020122f9f3d712636880fec8be4e
jpa198 <james.paterson@canterbury.ac.nz>
2019-01-24 10:36:04 +1300

    Added script to automate using a folder of folders containing ground failure hdf5 files and a folder of location csv files

---
+++ infrastructure/automate_infrastructure.sh
@@ -0,0 +1,25 @@
+imdb=$1
+h5_parent_dir=$2
+output=$3
+csv_folder=$4
+
+
+for dir in "$h5_parent_dir"/*; do
+    if test -d "$dir"; then
+        landslide=`ls -1 $dir/*_jessee_*.hdf5|head -1`
+        liquefaction=`ls -1 $dir/*_zhu_*.hdf5|head -1`
+        folder_name=`basename $dir`
+        echo $folder_name
+        output_folder="$output/$folder_name"
+        mkdir -p $output_folder
+        
+        echo $landslide
+        echo "$liquefaction"
+        echo "$output_folder"
+        echo "$csv_folder"
+        echo "$imdb"
+
+        python3 ./master_point_finder.py $imdb $landslide $liquefaction $output_folder $csv_folder
+    fi
+
+done
