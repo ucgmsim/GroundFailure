e0338843c1e85b1bff68a536e19be1ff1bc71dd4
jpa198 <james.paterson@canterbury.ac.nz>
2019-03-13 14:04:24 +1300

    Initial changes for multiple IM retrieval

--- infrastructure/imdb_point_finder.py
+++ infrastructure/imdb_point_finder.py
@@ -10,7 +10,7 @@ import argparse
 import pandas as pd
 
 
-def imdb_finder(imdb_file, input_file, output_file, realisation, intensity_measure):
+def imdb_finder(imdb_file, input_file, output_file, realisation, intensity_measures):
 
     data = pd.read_csv(input_file, index_col=0, encoding="ISO-8859-1")
     data = data.assign(CLOSEST_STATION="")
@@ -26,13 +26,15 @@ def imdb_finder(imdb_file, input_file, output_file, realisation, intensity_measu
             # Too far to be useful
             continue
         data.at[i, "CLOSEST_STATION"] = station_name
-        intensity_measure_realisations = imdb.station_ims(imdb_file, station_name)[
-            intensity_measure
-        ]
-        if realisation in intensity_measure_realisations:
-            data.at[i, "INTENSITY_MEASURE"] = intensity_measure_realisations[
-                realisation
+
+        for im in intensity_measures:
+            intensity_measure_realisations = imdb.station_ims(imdb_file, station_name)[
+                im
             ]
+            if realisation in intensity_measure_realisations:
+                data.at[i, "INTENSITY_MEASURE"] = intensity_measure_realisations[
+                    realisation
+                ]
 
     data.to_csv(output_file)
 
@@ -43,7 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("input", help="Input file name")
     parser.add_argument("output", help="Output file name")
     parser.add_argument("realisation", help="The realisation to choose")
-    parser.add_argument("im", help="Intensity measure name")
+    parser.add_argument("im", help="Intensity measure name", nargs="+", type=str)
     args = parser.parse_args()
 
     imdb_finder(args.imdb, args.input, args.output, args.realisation, args.im)
