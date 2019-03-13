c24b75dbe7e6c0dc799019a949156600ee0f2c25
jpa198 <james.paterson@canterbury.ac.nz>
2019-03-13 14:09:53 +1300

    Blacked

--- infrastructure/master_point_finder.py
+++ infrastructure/master_point_finder.py
@@ -15,7 +15,7 @@ from qcore import imdb
 
 
 def main(imdb_fname, landslide_fname, liquefaction_fname, files, ims, realisation=None):
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count()//2)
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() // 2)
 
     if imdb_fname != "None":
 
@@ -30,7 +30,11 @@ def main(imdb_fname, landslide_fname, liquefaction_fname, files, ims, realisatio
 
         for im in temp_ims:
             if im not in imdb_ims:
-                print("No im found for {}, please ensure you have correct spelling and capitalisation.".format(im))
+                print(
+                    "No im found for {}, please ensure you have correct spelling and capitalisation.".format(
+                        im
+                    )
+                )
                 ims.remove(im)
 
         for input_file in files:
@@ -38,36 +42,64 @@ def main(imdb_fname, landslide_fname, liquefaction_fname, files, ims, realisatio
             # Find the name of the realisation
             if realisation is None:
                 if landslide_fname != "None":
-                    realisation_prefix = landslide_fname.split("/")[-1].split("_")[0] + realisation_constant
+                    realisation_prefix = (
+                        landslide_fname.split("/")[-1].split("_")[0]
+                        + realisation_constant
+                    )
                 elif liquefaction_fname != "None":
-                    realisation_prefix = liquefaction_fname.split("/")[-1].split("_")[0] + realisation_constant
+                    realisation_prefix = (
+                        liquefaction_fname.split("/")[-1].split("_")[0]
+                        + realisation_constant
+                    )
                 else:
                     print("Unable to determine realisation, aborting.")
                     continue
-            restricted_realisations = \
-                [x for x in sims if x.startswith(realisation_prefix)]
+            restricted_realisations = [
+                x for x in sims if x.startswith(realisation_prefix)
+            ]
 
-            if len(restricted_realisations)>1:
-                print("Too many possible realisations for file '{}', not attempting to find imdb values".format(input_file))
+            if len(restricted_realisations) > 1:
+                print(
+                    "Too many possible realisations for file '{}', not attempting to find imdb values".format(
+                        input_file
+                    )
+                )
                 continue
 
             realisation = restricted_realisations[0]
 
-            #List of ims to use.
+            # List of ims to use.
             for im in ims:
-                output = os.path.join(args.output, os.path.basename(input_file.replace(".csv", "_"+im+".csv")))
-                pool.apply_async(imdb_point_finder.imdb_finder,
-                                 (imdb_fname, input_file, output, realisation.encode(), im.encode()))
+                output = os.path.join(
+                    args.output,
+                    os.path.basename(input_file.replace(".csv", "_" + im + ".csv")),
+                )
+                pool.apply_async(
+                    imdb_point_finder.imdb_finder,
+                    (imdb_fname, input_file, output, realisation.encode(), im.encode()),
+                )
 
     if landslide_fname != "None":
         for input_file in files:
-            output = os.path.join(args.output, os.path.basename(input_file.replace(".csv", "_landslide.csv")))
-            pool.apply_async(usgs_point_finder.ground_failure_finder, (landslide_fname, input_file, output))
+            output = os.path.join(
+                args.output,
+                os.path.basename(input_file.replace(".csv", "_landslide.csv")),
+            )
+            pool.apply_async(
+                usgs_point_finder.ground_failure_finder,
+                (landslide_fname, input_file, output),
+            )
 
     if liquefaction_fname != "None":
         for input_file in files:
-            output = os.path.join(args.output, os.path.basename(input_file.replace(".csv", "_liquefaction.csv")))
-            pool.apply_async(usgs_point_finder.ground_failure_finder, (liquefaction_fname, input_file, output))
+            output = os.path.join(
+                args.output,
+                os.path.basename(input_file.replace(".csv", "_liquefaction.csv")),
+            )
+            pool.apply_async(
+                usgs_point_finder.ground_failure_finder,
+                (liquefaction_fname, input_file, output),
+            )
 
     pool.close()
     pool.join()
@@ -76,20 +108,46 @@ def main(imdb_fname, landslide_fname, liquefaction_fname, files, ims, realisatio
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser('masterInfra')
-    parser.add_argument('imdb', type=str, help='Path to imdb file. May be None')
-    parser.add_argument('landslide', type=str, help='Path to landslide hdf5 file. May be None')
-    parser.add_argument('liquefaction', type=str, help='Path to liquefaction hdf5 file. May be None')
-    parser.add_argument('output', type=str, help='The folder to put all output files in.')
-    parser.add_argument('-i', '--intensity_measures', type=str, dest='ims', nargs='+', default=['PGA'], help='The intensity measures to take from imdb.')
+    parser = argparse.ArgumentParser("masterInfra")
+    parser.add_argument("imdb", type=str, help="Path to imdb file. May be None")
+    parser.add_argument(
+        "landslide", type=str, help="Path to landslide hdf5 file. May be None"
+    )
+    parser.add_argument(
+        "liquefaction", type=str, help="Path to liquefaction hdf5 file. May be None"
+    )
+    parser.add_argument(
+        "output", type=str, help="The folder to put all output files in."
+    )
+    parser.add_argument(
+        "-i",
+        "--intensity_measures",
+        type=str,
+        dest="ims",
+        nargs="+",
+        default=["PGA"],
+        help="The intensity measures to take from imdb.",
+    )
 
     files = parser.add_mutually_exclusive_group(required=True)
-    files.add_argument('-g','--infrastructure_folder', type=str, help='Name of the rupture - should be unique per simulation')
-    files.add_argument('-f', '--infrastructure_files', type=str, dest='files', nargs=argparse.REMAINDER, help='A list of infrastructure files to be processed' )
+    files.add_argument(
+        "-g",
+        "--infrastructure_folder",
+        type=str,
+        help="Name of the rupture - should be unique per simulation",
+    )
+    files.add_argument(
+        "-f",
+        "--infrastructure_files",
+        type=str,
+        dest="files",
+        nargs=argparse.REMAINDER,
+        help="A list of infrastructure files to be processed",
+    )
 
-    if ('-f' not in sys.argv) and ('-g' not in sys.argv) and len(sys.argv)>1:
+    if ("-f" not in sys.argv) and ("-g" not in sys.argv) and len(sys.argv) > 1:
         sys.argv.insert(-1, "-g")
-    
+
     print(sys.argv)
 
     args = parser.parse_args()
@@ -100,5 +158,5 @@ if __name__ == "__main__":
     else:
         folder = args.infrastructure_folder
         files = list(map(lambda x: os.path.join(folder, x), os.listdir(folder)))
-    
+
     main(args.imdb, args.landslide, args.liquefaction, files, args.ims)
